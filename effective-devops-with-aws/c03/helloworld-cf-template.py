b9b0fcfe1adface3b79295e0c7bd638f179752be
Chanseok Park <pcspapa@gmail.com>
2019-08-13 21:17:14 +0900

    txt to markdown

---
+++ effective-devops-with-aws/c03/helloworld-cf-template.py
@@ -0,0 +1,83 @@
+"""Generating ClouldFormation template."""
+from ipaddress import ip_network
+
+from ipify import get_ip
+
+from troposphere import (
+    Base64,
+    ec2,
+    GetAtt,
+    Join,
+    Output,
+    Parameter,
+    Ref,
+    Template
+)
+
+ApplicationPort = "3000"
+
+t = Template()
+
+t.set_description("Effective DevOps in AWS: HelloWorld web application")
+
+t.add_parameter(Parameter(
+    "KeyPair",
+    Description="Name of an existing EC2 KeyPair to SSH",
+    Type="AWS::EC2::KeyPair::KeyName",
+    ConstraintDescription="must be the name of an existing EC2 KeyPair.",
+))
+
+t.add_resource(ec2.SecurityGroup(
+    "SecurityGroup",
+    GroupDescription="Allow SSH and TCP/{} access".format(ApplicationPort),
+    SecurityGroupIngress=[
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="22",
+            ToPort="22",
+            CidrIp="0.0.0.0/0",
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort=ApplicationPort,
+            ToPort=ApplicationPort,
+            CidrIp="0.0.0.0/0",
+        ),
+    ],
+))
+
+ud = Base64(Join('\n', [
+    "#!/bin/bash",
+    "yum update -y",
+    "curl -sL https://rpm.nodesource.com/setup_12.x | bash -",
+    "yum -y install nodejs",
+    "wget http://bit.ly/2vESNuc -O /home/ec2-user/helloworld.js",
+    "wget https://raw.githubusercontent.com/pcspapa/books-Effective-DevOps-with-AWS/master/chapter02/helloworld.service -O /etc/systemd/system/helloworld.service",
+    "sudo systemctl start helloworld.service"
+]))
+
+t.add_resource(ec2.Instance(
+    "instance",
+    ImageId="ami-047f7b46bd6dd5d84",
+    InstanceType="t2.micro",
+    SecurityGroups=[Ref("SecurityGroup")],
+    KeyName=Ref("KeyPair"),
+    UserData=ud,
+))
+
+t.add_output(Output(
+    "InstancePublicIp",
+    Description="Public IP of our instance.",
+    Value=GetAtt("instance", "PublicIp"),
+))
+
+t.add_output(Output(
+    "WebUrl",
+    Description="Application endpoint",
+    Value=Join("", [
+        "http://", GetAtt("instance", "PublicDnsName"),
+        ":", ApplicationPort
+    ]),
+))
+
+print (t.to_json())
